eebf7f5e23b3ba6e4d7bca085fc1bae11cd43eed
Otilia Zvoristeanu <100197641+otiliazvoristeanu@users.noreply.github.com>
2025-02-24 10:37:03 +0200

    Create spiral.cpp

---
+++ src/spiral.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "spiral.h"
+
+using namespace std;
+
+// Function to print the matrix in spiral order (Students must complete this)
+void spiralPrint(int n, int mat[MAX][MAX]) {
+    // TODO: Implement the logic for spiral traversal
+}
